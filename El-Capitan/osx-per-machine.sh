cb74a5e2bf0d43b0a7eafd8a83f73ffb2e97b344
Grant Rettke <gcr@wisdomandwonder.com>
2015-12-26 21:16:21 -0600

    Pause after every step to allow for review.

--- El-Capitan/osx-per-machine.sh
+++ El-Capitan/osx-per-machine.sh
@@ -3,18 +3,22 @@ sudo scutil --set ComputerName orion
 sudo scutil --set HostName orion
 sudo scutil --set LocalHostName orion
 sudo defaults write 'com.apple.smb.server' NetBIOSName -string orion
+read -p "Press [Enter] key to continue..."
 # 291FDE06-DF44-4156-A013-B763A8727B00 ends here
 
 # [[file:provisioning.org::*Per%20Machine][2F39C8B9-CA25-4C94-8E77-AD348D5235A9]]
 sudo defaults write /Library/Preferences/com.apple.loginwindow.plist SHOWFULLNAME -bool true
+read -p "Press [Enter] key to continue..."
 # 2F39C8B9-CA25-4C94-8E77-AD348D5235A9 ends here
 
 # [[file:provisioning.org::*Per%20Machine][850816F3-B82E-4EE0-9895-6E99CB6A7593]]
 sudo defaults write /Library/Preferences/com.apple.loginwindow.plist LoginwindowText -string "
 All creativity is an extended form of a joke.
           — Alan Kay"
+read -p "Press [Enter] key to continue..."
 # 850816F3-B82E-4EE0-9895-6E99CB6A7593 ends here
 
 # [[file:provisioning.org::*Per%20Machine][DBE84671-1CC0-4DBA-AC7C-72F9EAC857A0]]
 sudo spctl --master-disable
+read -p "Press [Enter] key to continue..."
 # DBE84671-1CC0-4DBA-AC7C-72F9EAC857A0 ends here
