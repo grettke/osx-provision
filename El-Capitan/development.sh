8f120bc8c5970f4ce4b4e7f8680cfb0a3d6cc72d
Grant Rettke <gcr@wisdomandwonder.com>
2016-01-31 20:07:05 -0600

    Buf fix building file

--- El-Capitan/development.sh
+++ El-Capitan/development.sh
@@ -28,6 +28,10 @@ brew install emacs --with-cocoa --with-gnutls --with-imagemagick
 brew linkapps
 # CCF91EC0-F689-4279-972C-9F1D32C4C64B ends here
 
+# [[file:provisioning.org::*Sourcetree][7143E58E-A1B9-4A5A-B189-6E88AB0612D7]]
+brew cask install sourcetree
+# 7143E58E-A1B9-4A5A-B189-6E88AB0612D7 ends here
+
 # [[file:provisioning.org::*sml][CC15039B-B03A-4B3B-AFF4-AF483DDFE3A6]]
 brew install smlnj
 # CC15039B-B03A-4B3B-AFF4-AF483DDFE3A6 ends here
@@ -43,6 +47,10 @@ brew install bzr
 # [[file:provisioning.org::*mercurial][6C03B646-2683-4BB5-88F7-D8EB337B3596]]
 brew install hg
 # 6C03B646-2683-4BB5-88F7-D8EB337B3596 ends here
+
+# [[file:provisioning.org::*PyCharm][610E12B1-3C58-4326-BE81-18EF1F40D544]]
+brew cask install pycharm
+# 610E12B1-3C58-4326-BE81-18EF1F40D544 ends here
 
 # [[file:provisioning.org::*WebStorm][479B7BC3-9F35-4422-82C9-DE3722E4A4B6]]
 brew cask install webstorm
