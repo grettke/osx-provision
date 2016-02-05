7395c39dda8f1638bd0019fe9560946a47ccddf0
Grant Rettke <gcr@wisdomandwonder.com>
2016-02-04 18:24:44 -0600

    Install manually

--- El-Capitan/dev-ops.sh
+++ El-Capitan/dev-ops.sh
@@ -1,11 +1,3 @@
-# [[file:provisioning.org::*Carbon%20Copy%20Cloner][27354A58-46C4-426B-8483-A96F7B63342D]]
-if [ ! -d "/Library/Application Support/VMware Tools" ]; then
-    brew cask install carbon-copy-cloner
-else
-    echo "carbon-copy-cloner: Only install on hosts"
-fi
-# 27354A58-46C4-426B-8483-A96F7B63342D ends here
-
 # [[file:provisioning.org::*VMWare%20Fusion][2B059288-F12E-48E1-939F-1138117FDE4B]]
 if [ ! -d "/Library/Application Support/VMware Tools" ]; then
     brew cask install vmware-fusion
