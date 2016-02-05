7e9ec0218ba1d224cba13da34e4cc775a6444adc
Grant Rettke <gcr@wisdomandwonder.com>
2016-02-04 18:52:51 -0600

    Added Packer

--- El-Capitan/dev-ops.sh
+++ El-Capitan/dev-ops.sh
@@ -24,6 +24,14 @@ else
 fi
 # 165A046E-FFC9-4431-8150-CF9F4890EA24 ends here
 
+# [[file:provisioning.org::*Packer][5C1B39F1-FC9C-4992-B891-D6208168D5F1]]
+if [ ! -d "/Library/Application Support/VMware Tools" ]; then
+    brew cask install packer
+else
+    echo "packer: Only install on hosts"
+fi
+# 5C1B39F1-FC9C-4992-B891-D6208168D5F1 ends here
+
 # [[file:provisioning.org::*iPartition][D7F9F051-BE46-4E38-BE35-6E8CE4B5D727]]
 brew cask install ipartition
 # D7F9F051-BE46-4E38-BE35-6E8CE4B5D727 ends here
