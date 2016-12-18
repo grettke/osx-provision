c02bfb4de3217e9fd49357b846ec09b27c453726
Grant Rettke <gcr@wisdomandwonder.com>
2016-12-17 23:14:22 -0600

    GNUPlot /can/ use Lua

--- El-Capitan/19_tex.sh
+++ El-Capitan/19_tex.sh
@@ -3,5 +3,6 @@ brew cask install mactex
 # C4F43DC6-97F3-4237-9746-B40243C20950 ends here
 
 # [[file:provisioning.org::A6FDD2B6-412D-41A5-B58A-942C6540BB57][A6FDD2B6-412D-41A5-B58A-942C6540BB57]]
+brew install lua
 brew install gnuplot --with-latex --with-pdflib-lite --with-test --with-x11
 # A6FDD2B6-412D-41A5-B58A-942C6540BB57 ends here
