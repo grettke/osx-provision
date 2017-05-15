5b45fce522982d4bf88d62d6ff063fec53726b5a
Grant Rettke <gcr@wisdomandwonder.com>
2017-05-15 18:09:07 -0500

    Pull own pretty-mode

--- El-Capitan/18_org-mode.sh
+++ El-Capitan/18_org-mode.sh
@@ -1,6 +1,7 @@
 # [[file:~/git/github/osx-provision/El-Capitan/provisioning.org::org_gcr_2017-05-12_mara_9ED0592E-A409-4049-BD98-DD5151723C1A][org_gcr_2017-05-12_mara_9ED0592E-A409-4049-BD98-DD5151723C1A]]
 rm -rf ~/src/org-mode
 rm -rf ~/src/org2blog
+rm -rf ~/src/pretty-mode
 rm -rf ~/src/use-package
 cd ~/src
 echo `date +%Y-%m-%dT%H:%M:%S%z`
@@ -10,5 +11,6 @@ cd org-mode
 make autoloads
 cd ~/src
 git clone github-gcr:grettke/org2blog.git
+git clone github-gcr:grettke/pretty-mode.git
 git clone https://github.com/jwiegley/use-package.git
 # org_gcr_2017-05-12_mara_9ED0592E-A409-4049-BD98-DD5151723C1A ends here
