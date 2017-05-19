3fc9eb9e27c391795c819037692501f0ac268eee
Grant Rettke <gcr@wisdomandwonder.com>
2017-05-19 14:17:46 -0500

    Pull reveal.js

--- El-Capitan/18_org-mode.sh
+++ El-Capitan/18_org-mode.sh
@@ -3,6 +3,7 @@ rm -rf ~/src/org-mode
 rm -rf ~/src/org2blog
 rm -rf ~/src/pretty-mode
 rm -rf ~/src/use-package
+rm -rf ~/src/reveal.js
 cd ~/src
 echo `date +%Y-%m-%dT%H:%M:%S%z`
 git clone git://orgmode.org/org-mode.git
@@ -13,4 +14,5 @@ cd ~/src
 git clone github-gcr:punchagan/org2blog.git
 git clone github-gcr:grettke/pretty-mode.git
 git clone https://github.com/jwiegley/use-package.git
+git clone https://github.com/hakimel/reveal.js.git
 # org_gcr_2017-05-12_mara_9ED0592E-A409-4049-BD98-DD5151723C1A ends here
