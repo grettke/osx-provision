c55f336a6b92deb93394d3fdf14ce6d562975e2e
Grant Rettke <gcr@wisdomandwonder.com>
2017-06-26 13:31:20 -0500

    Use updated xml-rpc library

--- El-Capitan/18_org-mode.sh
+++ El-Capitan/18_org-mode.sh
@@ -1,4 +1,5 @@
 # [[file:~/git/github/osx-provision/El-Capitan/provisioning.org::org_gcr_2017-05-12_mara_9ED0592E-A409-4049-BD98-DD5151723C1A][org_gcr_2017-05-12_mara_9ED0592E-A409-4049-BD98-DD5151723C1A]]
+rm -rf ~/src/xml-rpc-el
 rm -rf ~/src/metaweblog
 rm -rf ~/src/org-mode
 rm -rf ~/src/org2blog
@@ -12,6 +13,7 @@ echo `date +%Y-%m-%dT%H:%M:%S%z`
 cd org-mode
 make autoloads
 cd ~/src
+git clone github-`whoami`:grettke/xml-rpc-el.git
 git clone github-gcr:org2blog/metaweblog.git
 git clone github-gcr:punchagan/org2blog.git
 git clone github-gcr:grettke/pretty-mode.git
