68b7b10407b2653a94dcf75abe646dcc6cb689f1
Grant Rettke <gcr@wisdomandwonder.com>
2017-08-23 20:42:59 -0500

    Use go-snippets from MELPA

--- El-Capitan/18_org-mode.sh
+++ El-Capitan/18_org-mode.sh
@@ -7,7 +7,6 @@ rm -rf ~/src/pretty-mode
 rm -rf ~/src/use-package
 rm -rf ~/src/reveal.js
 rm -rf ~/src/go-hydra
-rm -rf ~/src/go-snippets
 cd ~/src
 echo `date +%Y-%m-%dT%H:%M:%S%z`
 git clone git://orgmode.org/org-mode.git
@@ -22,5 +21,4 @@ git clone github-`whoami`:grettke/pretty-mode.git
 git clone https://github.com/jwiegley/use-package.git
 git clone https://github.com/hakimel/reveal.js.git
 git clone gitlab-`whoami`:grettke/go-hydra.git
-git clone https://github.com/toumorokoshi/go-snippets
 # org_gcr_2017-05-12_mara_9ED0592E-A409-4049-BD98-DD5151723C1A ends here
