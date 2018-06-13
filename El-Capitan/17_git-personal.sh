141bb1aa33b3940f9908b1193c41b00c139e274c
Grant Rettke <gcr@wisdomandwonder.com>
2018-06-12 22:35:10 -0500

    (nhncs) Clone languagetool.

--- El-Capitan/17_git-personal.sh
+++ El-Capitan/17_git-personal.sh
@@ -46,6 +46,7 @@ git clone https://github.com/encharm/Font-Awesome-SVG-PNG
 git clone https://github.com/dwyl/english-words.git
 git clone https://github.com/LearnWebCode/json-example.git
 git clone https://github.com/codefresh-io/yaml-examples.git
+git clone https://github.com/languagetool-org/languagetool.git
 # org_gcr_2017-05-12_mara_CF6E9DCD-5A6E-4335-93CF-2E03937C9C28 ends here
 
 # [[file:~/git/github/osx-provision/El-Capitan/provisioning.org::org_gcr_2017-05-12_mara_300013CB-D6E2-414E-9FBF-68D4581D971C][org_gcr_2017-05-12_mara_300013CB-D6E2-414E-9FBF-68D4581D971C]]
