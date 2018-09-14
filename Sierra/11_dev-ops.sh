376dfec9add30c8ca8105ed89fe38228e764275f
Grant Rettke <gcr@wisdomandwonder.com>
2018-09-13 21:35:13 -0500

    (rfns) Remove ansiblell terraforming

--- Sierra/11_dev-ops.sh
+++ Sierra/11_dev-ops.sh
@@ -18,13 +18,3 @@ fi
 # [[file:~/git/github/osx-provision/Sierra/provisioning.org::org_gcr_2017-05-12_mara_411A8284-5DDC-45B4-A0D1-37EE4B6F6CC5][org_gcr_2017-05-12_mara_411A8284-5DDC-45B4-A0D1-37EE4B6F6CC5]]
 brew cask install ipartition
 # org_gcr_2017-05-12_mara_411A8284-5DDC-45B4-A0D1-37EE4B6F6CC5 ends here
-
-# [[file:~/git/github/osx-provision/Sierra/provisioning.org::org_gcr_2018-01-03_mara_209AB55E-7801-42AE-87ED-AD2E2AF61C4F][org_gcr_2018-01-03_mara_209AB55E-7801-42AE-87ED-AD2E2AF61C4F]]
-brew install terraform
-# org_gcr_2018-01-03_mara_209AB55E-7801-42AE-87ED-AD2E2AF61C4F ends here
-
-# [[file:~/git/github/osx-provision/Sierra/provisioning.org::org_gcr_2018-01-03_mara_51C9C084-72E7-48EC-8E72-9638C180A086][org_gcr_2018-01-03_mara_51C9C084-72E7-48EC-8E72-9638C180A086]]
-brew install ansible
-brew install ansible-lint
-brew install terraform-provisioner-ansible
-# org_gcr_2018-01-03_mara_51C9C084-72E7-48EC-8E72-9638C180A086 ends here
