8c3d1315d3fd99bd8dc7c33c4756d9804458ca76
Grant Rettke <gcr@wisdomandwonder.com>
2019-03-26 22:55:45 -0500

    (rfns) Switch to correct org branch

--- Sierra/18_org-mode.sh
+++ Sierra/18_org-mode.sh
@@ -3,8 +3,9 @@ rm -rf ~/src/org-mode
 cd ~/src
 echo `date +%Y-%m-%dT%H:%M:%S%z`
 git clone https://code.orgmode.org/bzg/org-mode.git
+cd org-mode
 git fetch && git fetch --tags
 echo `date +%Y-%m-%dT%H:%M:%S%z`
-cd org-mode
-make autoloads
+git checkout release_9.0.9
+mk autoloads
 # org_gcr_2017-05-12_mara_9ED0592E-A409-4049-BD98-DD5151723C1A ends here
