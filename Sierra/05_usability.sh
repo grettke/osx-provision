4c97b305b678a80f9d237d50e5c3654425c7c01a
Grant Rettke <gcr@wisdomandwonder.com>
2019-06-01 18:40:03 -0500

    (crrcts) free file sync

--- Sierra/05_usability.sh
+++ Sierra/05_usability.sh
@@ -70,6 +70,10 @@ brew cask install contexts
 brew cask install coconutbattery
 # org_gcr_2019-04-24T18-13-26-05-00_cosmicality_6C4FB56D-9B54-4029-992C-FC0A2BBD08F4 ends here
 
+# [[file:~/git/github/osx-provision/Sierra/provisioning.org::org_gcr_2019-05-29T21-58-33-05-00_cosmicality_A5FD8FC7-333B-4B0B-9876-202C6375DBC9][org_gcr_2019-05-29T21-58-33-05-00_cosmicality_A5FD8FC7-333B-4B0B-9876-202C6375DBC9]]
+brew cask install freefilesync
+# org_gcr_2019-05-29T21-58-33-05-00_cosmicality_A5FD8FC7-333B-4B0B-9876-202C6375DBC9 ends here
+
 # [[file:~/git/github/osx-provision/Sierra/provisioning.org::org_gcr_2017-05-12_mara_CFE8C328-2D5E-4367-9E36-8E76EF29F2BB][org_gcr_2017-05-12_mara_CFE8C328-2D5E-4367-9E36-8E76EF29F2BB]]
 echo "The Administration is complete."
 echo "Restart your Terminal to pick up the changes."
